0d2d3f359be0b16aa73ec77b50b1dcd0370e2025
Helbri <helene.brissard63@gmail.com>
2024-05-31 18:48:26 +0200

    push readme et script step 6

---
+++ script.js
@@ -0,0 +1,8 @@
+// la variable character est déclarée, et pour son initialisation, la valeur Hello lui est assignée
+let character = "Hello";
+// on fait afficher la valeur de la variable character dans la console (Hello)
+console.log(character);
+// la valeur World est assignée à la variable character
+character = "World";
+// on fait afficher la valeur de la variable character dans la console (World)
+console.log(character);
