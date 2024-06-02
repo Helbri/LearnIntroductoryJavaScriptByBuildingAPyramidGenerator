b819af8202547713359952d84af0b57cdb1dd32f
Helbri <helene.brissard63@gmail.com>
2024-06-02 13:26:27 +0200

    push script step 15

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-// déclaration variable character et initialisation avec 'Hello'
+// déclaration variable character et initialisation avec 'Hello' (quote)
 let character = "Hello";
+// déclaration variable count et initialisation avec la valeur 8 (pas de quote)
+let count = 8;
+// on regarde dans la console la valeur de la variable count +1
+console.log(count + 1);
 
-// déclaration variable profession et initialisation avec 'teacher'
-let profession = "teacher";
-// déclaration variable age
-let age;
-// consol.log pour connaître la valeur de la variable profession
-console.log(profession);
-// consol.log pour connaître la valeur de la variable age
-console.log(age);
-
-// step 12
+// step 15
