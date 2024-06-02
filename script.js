5e419c08e286837d9139f89268f709ced1f5d083
Helbri <helene.brissard63@gmail.com>
2024-06-02 13:19:41 +0200

    push script  step 9

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// la variable character est déclarée, et pour son initialisation, la valeur Hello lui est assignée
-let character = "Hello";
-// on fait afficher la valeur de la variable character dans la console (Hello)
+// déclaration variable character et initialisation avec 'Hello'
+let character = 'Hello';
+// console.log de la variable character pour afficher ce qu'elle contient dans la console JavaScript (Hello)
 console.log(character);
-// la valeur World est assignée à la variable character
+// nouvelle assignation de chaîne à la variable character ("World")
 character = "World";
-// on fait afficher la valeur de la variable character dans la console (World)
+// console.log de la variable character pour afficher ce qu'elle contient dans la console JavaScript (World)
 console.log(character);
+// déclaration variable secondCharacter
+let secondCharacter;
+// console.log de la variable secondCharacter pour afficher ce qu'elle contient (montre "undefined")
+console.log(secondCharacter);
+// assignation de la valeur Test à la variable secondCharacter
+secondCharacter = "Test";
+// step 9
