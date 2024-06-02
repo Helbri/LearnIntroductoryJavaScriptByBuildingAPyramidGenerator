7ad3e1f87d89f289725ebbfb8ca07b34b6a2e983
Helbri <helene.brissard63@gmail.com>
2024-06-02 18:15:16 +0200

    push script step 20

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 let character = "Hello";
 // déclaration variable count et initialisation avec la valeur 8 (pas de quote)
 let count = 8;
-// déclaration variable rows contenant un tableau vide
-let rows = [];
-
-// step 17
+// déclaration variable rows contenant un tableau avec des chaînes
+let rows = ["Naomi", "Quincy", "CamperChan"];
+// impression de la première valeur contenue dans le tableau rows
+console.log(rows[0]);
+// la valeur 10 est assignée à l'index 2
+rows[2] = 10;
+// impression de l'ensemble des valeurs du tableau rows
+console.log(rows);
+// step 20
