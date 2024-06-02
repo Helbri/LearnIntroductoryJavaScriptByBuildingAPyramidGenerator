5c2383d3c3f04c80c7895d8c5d5f0e32c0aa83ff
Helbri <helene.brissard63@gmail.com>
2024-06-02 13:24:52 +0200

    push script step 12

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // déclaration variable character et initialisation avec 'Hello'
 let character = "Hello";
 
-// step 11
+// déclaration variable profession et initialisation avec 'teacher'
+let profession = "teacher";
+// déclaration variable age
+let age;
+// consol.log pour connaître la valeur de la variable profession
+console.log(profession);
+// consol.log pour connaître la valeur de la variable age
+console.log(age);
+
+// step 12
