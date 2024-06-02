01b3737ad678bf227873a01a3ec3573425eb13c2
Helbri <helene.brissard63@gmail.com>
2024-06-02 13:24:03 +0200

    push script step 11

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 // déclaration variable character et initialisation avec 'Hello'
 let character = "Hello";
-// console.log de la variable character pour afficher ce qu'elle contient dans la console JavaScript (Hello)
-console.log(character);
-// nouvelle assignation de chaîne à la variable character ("World")
-character = "World";
-// console.log de la variable character pour afficher ce qu'elle contient dans la console JavaScript (World)
-console.log(character);
-// déclaration variable secondCharacter
-let secondCharacter;
-// console.log de la variable secondCharacter pour afficher ce qu'elle contient (montre "undefined")
-console.log(secondCharacter);
-// assignation de la valeur de la variable character à la variable secondCharacter
-secondCharacter = character;
-// step 10
+
+// step 11
