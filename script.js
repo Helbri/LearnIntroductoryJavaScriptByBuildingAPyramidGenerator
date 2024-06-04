f381ec62e84af8dd887e9ddb2dfa6b023ba2054c
Helbri <helene.brissard63@gmail.com>
2024-06-04 19:40:27 +0200

    push script step 37

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // déclaration variable character et initialisation avec "#" (quote)
-let character = "#";
+const character = "#";
 // déclaration de la variable count et initialisation avec la valeur 8 (pas de quote)
-let count = 8;
+const count = 8;
 // déclaration de la variable rows contenant un tableau vide
-let rows = "";
+const rows = [];
 
-// step 29
+// une boucle for a été déclarée. i vaut 0, il doit être inférieur à 8 et i est augmenté de 1
+for (let i = 0; i < count; i = i + 1) {
+  // i est ajouté sur à la fin de chaque rangée de tableau rows
+  rows.push(i);
+}
+
+// déclaration de la variable result
+let result = "";
+// impression du résultat de result dans la console
+console.log(result);
+
+// step 37
