9dfbc3fc01cc142a53bb5837bd740265478d2e4a
Helbri <helene.brissard63@gmail.com>
2024-06-05 15:35:26 +0200

    push script stpush script step 40

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-// déclaration variable character et initialisation avec "Hello" (quote)
-let character = "Hello";
+// déclaration variable character et initialisation avec "#" (quote)
+const character = "#";
 // déclaration de la variable count et initialisation avec la valeur 8 (pas de quote)
-let count = 8;
-// déclaration de la variable rows contenant un tableau avec des chaînes
-let rows = ["Naomi", "Quincy", "CamperChan"];
-// la méthode .push() permet l'ajout d'une chaîne à la fin du tableau ("freeCodeCamp")
-rows.push("freeCodeCamp");
-// la variable pushed prends en assignation le résultat de la méthode push sur l'objet rows. Ajoute un élément après le dernier élément du tableau.
-let pushed = rows.push("freeCodeCamp");
-// permet de voir la nouvelle longueur du tableau après l'ajout
-console.log(pushed);
-// la variable popped prends en assignation le résultat de la méthode pop sur l'objet rows. Supprime le dernier élément du tableau et retourne l'élément supprimé.
-let popped = rows.pop();
-// affiche dans la console le résultat de la variable popped
-console.log(popped);
-// affiche dans la console le contenu actuel de la variable rows
-console.log(rows);
-// step 25
+const count = 8;
+// déclaration de la variable rows contenant un tableau vide
+const rows = [];
+
+// une boucle for a été déclarée. i vaut 0, il doit être inférieur à 8 et i est augmenté de 1
+for (let i = 0; i < count; i = i + 1) {
+  // i est ajouté sur à la fin de chaque rangée de tableau rows
+  rows.push(i);
+}
+
+// déclaration de la variable result
+let result = "";
+// impression du résultat de result dans la console
+console.log(result);
+
+// utilisation d'une boucle for...of pour parcourir le tableau rows avec la variable row.
+for (const row of rows) {
+  // le résultat de la valeur result est concaténée avec le + au caractère d'échappement \n et la variable row sont réassigné à la variable result
+  result = result + "\n" + row;
+}
+// step 40
